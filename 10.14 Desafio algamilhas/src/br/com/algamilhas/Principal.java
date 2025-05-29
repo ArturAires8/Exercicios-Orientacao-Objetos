a96bfb61781d3058eb31f40f403896945f52a230
Artur Aires <166089713+ArturAires8@users.noreply.github.com>
2025-05-28 21:20:05 -0300

    Add files via upload

---
+++ 10.14 Desafio algamilhas/src/br/com/algamilhas/Principal.java
@@ -0,0 +1,13 @@
+package br.com.algamilhas;
+
+
+import br.com.algamilhas.pontuacao.Participante;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Participante participante = new Participante("Artur");
+
+
+    }
+}
